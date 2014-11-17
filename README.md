2f7cf68c410b4ae85c37b55760fcda9142665eed
liaoxuezhi <liaoxuezhi@baidu.com>
2014-11-17 14:03:45 +0800

    添加更多的 demo

--- README.md
+++ README.md
@@ -11,3 +11,21 @@ Fis amd demo
 npm install fis-postprocessor-amd -g
 npm install fis-postpackager-autoload -g
 ```
+
+## 兼容性问题
+
+除了 `require.async` 外，其他写法都兼容。其实原来的写法 `require.async(id | deps, callback)` 等价于 `require(deps, callback)`，不过也没关系，fis-postprocessor-amd 会自动纠正这种用法。
+
+另外不在 `define` 中的 require(id) 这种用法，不是 amd 的标准用法，但是有很多老用户是这么写的，所以做了兼容处理，但是不推荐这么用, 因为这样你是没法把 mod-amd.js 直接换成 require.js 使用。
+
+## 注意
+
+由于 FIS 产出的 module id 为这种格式 ns:path/xxxx, require.js 认为它是 url 导致发送 script 请求而报错。
+
+需要对 require.js 做修改，直接在最下面加上如下配置
+
+```javascript
+// 修改 require.js
+// 由于 fis 中生成的 module id 格式为 xxx:path 这种格式，被认为是 url 导致 require.js 会发请求
+require.jsExtRegExp = /^\/|:\/\/|\?|\.js$/;
+```
--- fis-conf.js
+++ fis-conf.js
@@ -1,3 +1,4 @@
+
 // 开起 autuload, 好处是，依赖自动加载。
 fis.config.set('modules.postpackager', 'autoload');
 fis.config.set('settings.postpackager.autoload.type', 'requirejs');
@@ -7,26 +8,39 @@ fis.config.set('modules.postprocessor.html', 'amd');
 fis.config.set('modules.postprocessor.js', 'amd');
 fis.config.set('settings.postprocessor.amd', {
     baseUrl: '.',
+
+    // 查看：https://github.com/amdjs/amdjs-api/blob/master/CommonConfig.md#paths-
+    // 不同的是，这是编译期处理，路径请填写编译路径。
     paths: {
-        app: './modules/app'
+        jquery: 'modules/libs/jquery/jquery.js',
+        bootstrap: 'modules/libs/bootstrap/js/bootstrap.js',
+        app: './modules/app',
+        css: './modules/css.js'
     },
+
+    // 查看：https://github.com/amdjs/amdjs-api/blob/master/CommonConfig.md#packages-
+    // 不同的是，这是编译期处理，路径请填写编译路径。
     packages: [
 
-        // zrender 模块下的
         {
             name: 'zrender',
             location: 'modules/libs/zrender',
             main: 'zrender'
         },
 
-        // 本模块下的
-
         {
             name: 'echarts',
             location: 'modules/libs/echarts',
             main: 'echarts'
         }
-    ]
+    ],
+
+    // 设置 bootstrap 依赖 jquery
+    // 更多用法见：https://github.com/amdjs/amdjs-api/blob/master/CommonConfig.md#shim-
+    // key 为编译期路径。
+    shim: {
+        'modules/libs/bootstrap/js/bootstrap.js': ['jquery']
+    }
 });
 
 // 使用 depscombine 是因为，在配置 pack 的时候，命中的文件其依赖也会打包进来。
@@ -38,3 +52,14 @@ fis.config.set('pack', {
     'pkg/zrender.js': ['modules/libs/zrender/zrender.js'],
     'pkg/echarts.js': ['modules/libs/echarts/echarts.js']
 });
+
+fis.config.set('roadmap.path', [
+
+    // 标记 isMod 为 true, 这样，在 modules 里面的满足 commonjs 规范的 js 会自动包装成 amd js, 以至于能在浏览器中运行。
+    //
+    {
+        reg: /^\/modules\/(.*\.js)$/i,
+        isMod: true,
+        release: '/modules\/$1'
+    }
+]);
